1bf24b697a0f8347a211eff03bdf70c6fe2c3386
AntonT <Grasmeister@list.ru>
2022-07-13 23:34:39 +0300

    2.2.2 version 2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +8,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import web.Service.CarServiceImpl;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
+    @Autowired
     private CarServiceImpl service = new CarServiceImpl();
     private List<Car> cars = service.addCars();
 
+//    @Autowired
+//    public CarController(CarServiceImpl service) {
+//        this.service = service;
+//    }
 
     @GetMapping(value = "/cars")
 //    public String cars(Model model) {
@@ -25,15 +30,13 @@ public class CarController {
 //    @GetMapping(value = "/cars?count=?")
     public String carsCounter(Model model, @RequestParam(value = "count", required = false) String count) {
 
-        if(count != null){
+        if (count != null) {
             int x = Integer.parseInt(count);
             cars = service.carCountList(x);
         }
         model.addAttribute("cars", cars);
         return "cars";
     }
-
-
 
 
 }
